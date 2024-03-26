2843ef149d05bdae11d302092d42ad62bb1e63a1
singsingkim <kimyc2661@naver.com>
2024-03-26 10:10:51 +0900

    123

--- tf114/tf01_hello.py
+++ tf114/tf01_hello.py
@@ -3,6 +3,7 @@ print(tf.__version__)   # 1.14.0
 
 print('hello world')
 
+# 텐서1 에서 중요한 세 가지
 # 컨스턴트(상수), 배리어블(변수), 프레이스홀드
 hello = tf.constant('hello world')
 # Tensor("Const:0", shape=(), dtype=string)
